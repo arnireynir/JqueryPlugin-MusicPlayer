e103a566d042468cb78a205c84191290b36e6fdc
Arni Reynir <arni.reynir89@gmail.com>
2012-02-20 17:06:54 +0000

    Finishing prjo setup and beginning of modinizer

---
+++ HTML5 MusicPlayer/musicplayer.js
@@ -0,0 +1,41 @@
+/*WEPO - Skilaverkefni 4*/
+/*Árni Reynir & Viktor Blöndal*/
+
+
+//This is to keep conflit to minninum.
+(function($){
+	//To attack method to jQuery
+	$.fn.musicplayer = function() {
+		
+		//This is the plugin name
+		awemusic: function() {
+			//Write code here.... with modernizr example	
+			/* if(Modernizr.audio){
+      			alert("Your browser supports audio tag, enjoy!!");
+   			   }else{
+      			alert("Sorry, browser does not support audio tag!");
+   			}*/
+			
+		} 	
+	};
+	 
+	
+})(Jquery);
+
+/* Creating html5 code */
+/*
+document.createElement( "div" )
+ or $("<div>")
+ even $( document.createElement("<div"))
+
+
+To start htmltag after body(target) t.div
+$( newElement ).appendTo( target );
+or
+$ (target).append( newElement );
+
+Setting attribute : 
+domElement.settAtrribute(name,value);
+$(selector).attr(attrName, attrValue);
+
+*/
